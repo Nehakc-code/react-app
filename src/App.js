d8e44be66ca261f7d43ac885eb522f0178a69231
Nehakc-code <nehakc0@gmail.com>
2022-10-11 16:21:10 +0530

    Active step state added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Stepper from "./components/Stepper";
-
+import{createContext,useState} from "react";
+export const FormContext = createContext();
 function App() {
+  const[activeStep,setActiveStep]=useState(0);
   return (
+
+    <FormContext.Provider value={{activeStep,setActiveStep}}>
     <div className="w-screen h-screen flex flex-col items-center justify-start">
       <Stepper />
     </div>
+    </FormContext.Provider>
   );
 }
 
--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -1,6 +1,17 @@
 import React, { useContext, useEffect } from "react";
-
+import {FormContext} from "../App";
 function Stepper() {
+  const{activeStep} = useContext(FormContext);
+  useEffect(() => {
+    const stepperItems=document.querySelectorAll(".stepper-item");
+    stepperItems.forEach((step,i)=>{
+      if(i<=activeStep) {
+        step.classList.add("bg-indigo-500","text-white");
+      }else{
+        step.classList.remove("bg-indigo-500","text-white");
+      }
+    });
+  },[activeStep]);
   return (
     <div className="w-2/3 flex flex-row items-center justify-center px-32 py-16">
       <div className="stepper-item w-8 h-8 text-center font-medium border-2 rounded-full">
