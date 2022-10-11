9c11a175e3037a911e55d3e93cb6501ca8e27ba1
Pooja Chavan <poojakchavan02@gmail.com>
2022-10-11 12:15:23 +0530

    Dependencies added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Stepper from "./components/Stepper";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-start">
+      <Stepper />
+    </div>
+  );
+}
+
+export default App;
